1f68b8e90907ac90230c6df924ef56bd27f269da
IonasAndreea <ionasandreeag@gmail.com>
2024-04-16 09:17:02 +0300

    Test Client

--- demo/src/test/java/com/example/demo/testing/ClientServiceTest.java
+++ demo/src/test/java/com/example/demo/testing/ClientServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.testing;
 
 import com.example.demo.entity.Clients;
+import com.example.demo.entity.Products;
 import com.example.demo.repositories.ClientRepository;
 import com.example.demo.services.ClientsService;
 import org.junit.Before;
@@ -9,7 +10,12 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 public class ClientServiceTest {
@@ -31,5 +37,36 @@ public class ClientServiceTest {
         this.clientsService.createClient(client);
         Mockito.verify(this.clientRepositoryMock).save(client);
     }
+
+    @Test
+    public void TestGetAll(){
+        Clients client1 = new Clients(30, "ElenaPop", "ema@pop.com", "1639*pop", "Cluj", false);
+        Clients client2 = new Clients(33, "Mara","mara@here.com", "mar996523", "Cluj", false);
+        List<Clients> clientsList = List.of(client1, client2);
+        when(this.clientRepositoryMock.findAll()).thenReturn(clientsList);
+        List<Clients> res = this.clientsService.getAllClients();
+        Mockito.verify(this.clientRepositoryMock).findAll();
+        assertEquals(clientsList, res);
+
+    }
+
+    @Test
+    public void testGetById() {
+        Clients client = new Clients(33, "Mara","mara@here.com", "mar996523", "Cluj", false);
+        when(this.clientRepositoryMock.findById(client.getId())).thenReturn(Optional.of(client));
+        Optional<Clients> res = this.clientsService.getClientsById(client.getId());
+        Mockito.verify(this.clientRepositoryMock).findById(client.getId());
+        assertTrue(res.isPresent());
+        assertEquals(client, res.get());
+    }
+
+    @Test
+    public void testDelete() {
+        long clientId = 10;
+        doNothing().when(this.clientRepositoryMock).deleteById(clientId);
+        this.clientsService.deleteClient(clientId);
+        Mockito.verify(this.clientRepositoryMock).deleteById(clientId);
+    }
+
 
 }
